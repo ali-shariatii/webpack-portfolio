b82a7b9491b758dcac3c50d839cb579ed013948f
ali.shariati <a.shariatii91@gmail.com>
2020-07-21 23:32:55 -0400

    minified assets/img

--- webpack.common.js
+++ webpack.common.js
@@ -11,16 +11,43 @@ module.exports = {
             },
             {
                 test: /\.(svg|png|jpg|gif)$/i,
-                use: {
-                    loader: "file-loader",
+                use: [
+                    {
+                    loader: 'file-loader',
                     options: {
                         name: "[name].[hash].[ext]",
-                        outputPath: "assets/img"
+                        outputPath: "assets/img",
                     }
-                }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            //bypassOnDebug: true, // webpack@1.x
+                            //disable: true, // webpack@2.x and newer
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 50
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.50, 0.50],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 60
+                            }
+                        },
+                    },
+                ]
             },
             {
-                test: /\.(woff|woff2|eot|ttf|otf|jpe?g)$/,
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use:  {                   
                     loader: "file-loader",
                     options: {
